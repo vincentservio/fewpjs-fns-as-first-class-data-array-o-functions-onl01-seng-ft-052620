c561397b11e9502e0045c57191bd5e52c57dafac
Vincent Servio <vincentservio@gmail.com>
2020-08-26 00:31:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+const wakeDog = (dogName,dogBreed) => {
+  return(`Wake ${dogName} the ${dogBreed}`);
+}
+
+const leashDog = (dogName,dogBreed) => {
+  return(`Leash ${dogName} the ${dogBreed}`);
+}
+
+const walkToPark = (dogName,dogBreed) => {
+  return(`Walk to the park with ${dogName} the ${dogBreed}`);
+}
+
+const throwFrisbee = (dogName,dogBreed) => {
+  return(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+}
+const walkHome = (dogName,dogBreed) => {
+  return(`Walk home with ${dogName} the ${dogBreed}`);
+}
+const unleashDog = (dogName,dogBreed) => {
+  return(`Unleash ${dogName} the ${dogBreed}`);
+}
+
+const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, 
+unleashDog]
+
+  
+const exerciseDog = (dogName, dogBreed) => {
+ return routine.map(e => e(dogName, dogBreed))
+}
